b3b4988e08f897a1769cd637aa62bea8a5b0bcf3
GiovanniPenha <giovanni.lpenha@gmail.com>
2026-03-25 19:05:30 -0300

    Atualizando...

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -7,8 +7,10 @@ import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -37,14 +39,26 @@ public class Main {
            temporadas.add(dadosTemporada);
        }
        temporadas.forEach(System.out::println);
-       /*
+
        for(int i = 0; i < dados.totalTemporadas(); i++){
            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
            for(int n = 0; n < episodiosTemporada.size(); n++){
                System.out.println("S" + (i+1) + " Ep." + (n+1) + ": " + episodiosTemporada.get(n).titulo());
            }
        }
-        */
-       temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+       //temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+       List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+               .flatMap(t -> t.episodios().stream())
+               .collect(Collectors.toList());
+
+       System.out.println("\n5 melhores episódios: ");
+
+       dadosEpisodios.stream()
+               .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+               .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+               .limit(5)
+               .forEach(System.out::println);
    }
 }
