2f9b80ce2145af77affeeacdbc21e5c34e37730d
GiovanniPenha <giovanni.lpenha@gmail.com>
2026-03-25 12:51:45 -0300

    Atualizando...

---
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -0,0 +1,50 @@
+package br.com.alura.screenmatch.main;
+
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=54aea7e6";
+
+    private ConsumoApi consumo = new ConsumoApi();
+
+    private ConverteDados conversor = new ConverteDados();
+
+   public void exibeMenu(){
+       System.out.println("Buscar série: ");
+       var nomeSerie = scanner.nextLine();
+       var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+
+       DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+       System.out.println(dados);
+
+       List<DadosTemporada> temporadas = new ArrayList<>();
+
+       for (int i = 1; i <= dados.totalTemporadas(); i++){
+           json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+           DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+           temporadas.add(dadosTemporada);
+       }
+       temporadas.forEach(System.out::println);
+       /*
+       for(int i = 0; i < dados.totalTemporadas(); i++){
+           List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+           for(int n = 0; n < episodiosTemporada.size(); n++){
+               System.out.println("S" + (i+1) + " Ep." + (n+1) + ": " + episodiosTemporada.get(n).titulo());
+           }
+       }
+        */
+       temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+   }
+}
